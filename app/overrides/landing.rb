222083ad07ff2707a8e688ea0e4f49e6159175c2
Anjan Chatterjee <theanjanchatterjee@gmail.com>
2017-01-31 22:42:19 +0530

    Initial commit

---
+++ app/overrides/landing.rb
@@ -0,0 +1,5 @@
+Deface::Override.new(virtual_path: 'spree/home/index',
+        replace: '[data-hook="homepage_products"]',
+        name: 'landing-products',
+        partial: 'home/landing',
+        original: '461aae32b5912b8551fcf3a823427507f434a0cc')
